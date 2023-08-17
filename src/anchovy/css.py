6d4c5b0e3d0faeb40156e717c18baa7fe0d468dd
Daniel Foerster <pydsigner@gmail.com>
2023-08-17 14:34:29 -0500

    Move CSS parser to its own package (#32)

--- src/anchovy/css.py
+++ src/anchovy/css.py
@@ -2,8 +2,8 @@ from __future__ import annotations
 
 from pathlib import Path
 
-from ..core import Step
-from ..dependencies import PipDependency
+from .core import Step
+from .dependencies import PipDependency
 
 
 class AnchovyCSSStep(Step):
@@ -15,13 +15,13 @@ class AnchovyCSSStep(Step):
     @classmethod
     def get_dependencies(cls):
         return {
-            PipDependency('tinycss2'),
+            PipDependency('anchovy_css'),
         }
 
     def __call__(self, path: Path, output_paths: list[Path]):
         if not output_paths:
             return
-        from .parser import process
+        from anchovy_css import process
         processed = process(path.read_text(self.encoding))
         for target_path in output_paths:
             target_path.parent.mkdir(parents=True, exist_ok=True)
