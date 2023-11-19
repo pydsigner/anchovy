1c652348dc5ada6d4d66d667c8f3ad9850952d70
Daniel Foerster <pydsigner@gmail.com>
2023-11-19 14:50:27 -0600

    Update examples/code_index.py to demonstrate how manage staleness for indexes (#65)

--- examples/code_index.py
+++ examples/code_index.py
@@ -7,8 +7,9 @@ from anchovy import (
     AnchovyCSSStep,
     AssetMinifierStep,
     CSSMinifierStep,
+    Context,
+    CustodyEntry,
     DirectCopyStep,
-    Matcher,
     InputBuildSettings,
     JinjaExtendedMarkdownStep,
     JinjaRenderStep,
@@ -21,7 +22,6 @@ from anchovy import (
     UnpackArchiveStep,
     WorkingDirPathCalc,
 )
-from anchovy.custody import CustodyEntry
 
 if t.TYPE_CHECKING:
     from jinja2 import Environment
@@ -55,41 +55,53 @@ class CodeIndexStep(JinjaRenderStep):
     encoding = 'utf-8'
 
     def __init__(self,
-                 leaf_matcher: Matcher,
-                 leaf_calc: PathCalc,
+                 leaf_glob: str,
+                 leaf_calc: PathCalc[None],
                  env: Environment | None = None,
                  extra_globals: dict[str, t.Any] | None = None):
         super().__init__(env, extra_globals)
-        self.leaf_matcher = leaf_matcher
+        self.leaf_glob = leaf_glob
         self.leaf_calc = leaf_calc
 
     @classmethod
     def get_dependencies(cls):
         return super().get_dependencies()
 
+    def bind(self, context: Context):
+        super().bind(context)
+        @context.custodian.register_checker('glob_manifest', override=False)
+        def glob_manifest_stale(entry: CustodyEntry):
+            path, glob = entry.key.rsplit(':', 1)
+            children = {context.custodian.degenericize_path(p) for p in entry['files']}
+            parent = context.custodian.degenericize_path(path)
+            return set(parent.glob(glob)) == children
+
     def __call__(self, path: Path, output_paths: list[Path]):
-        matched_paths: list[Path] = [path]
+        matched_paths: list[Path] = []
         leaves: list[tuple[Path, Path]] = []
-        for sibling in path.parent.iterdir():
-            print('sibling:', sibling)
+        for sibling in path.parent.glob(self.leaf_glob):
             if sibling == path or sibling.is_dir():
                 continue
-            print('sibling matched thus')
-            if match := self.leaf_matcher(self.context, sibling):
-                print('sibling matched', match)
-                matched_paths.append(sibling)
-                leaves.append((
-                    sibling,
-                    self.leaf_calc(self.context, sibling, match).relative_to(self.context['output_dir']),
-                ))
+            matched_paths.append(sibling)
+            leaves.append((
+                sibling,
+                self.leaf_calc(self.context, sibling, None).relative_to(self.context['output_dir']),
+            ))
 
-        print('leaves:', leaves)
         self.render_template(
             path.relative_to(self.context['working_dir']).as_posix(),
             {'leaves': leaves},
             output_paths
         )
-        return matched_paths, output_paths
+        centry = CustodyEntry(
+            'glob_manifest',
+            # ':' shouldn't appear in globs. If it could, resort to a more
+            # bulletproof though uglier way to store this compound key as a
+            # string.
+            f'{self.context.custodian.genericize_path(path.parent)}:{self.leaf_glob}',
+            {'files': [self.context.custodian.genericize_path(p) for p in matched_paths]}
+        )
+        return [path, centry, *matched_paths], output_paths
 
 
 # Optional, and can be overridden with CLI arguments.
@@ -140,7 +152,7 @@ RULES = [
         REMatcher(r'index(?P<ext>\.jinja\.html)', parent_dir='working_dir'),
         [WorkingDirPathCalc('.html'), None],
         CodeIndexStep(
-            REMatcher(r'.*\.py'),
+            '*.py',
             OutputDirPathCalc('.html')
         )
     ),
