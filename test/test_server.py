dce5e94c93d6e45f3060846e33aab945a1625596
Daniel Foerster <pydsigner@gmail.com>
2023-12-21 13:28:01 -0600

    More anchovy.server tests (#57)

--- test/test_server.py
+++ test/test_server.py
@@ -1,9 +1,12 @@
+import contextlib
 import pathlib
+import socket
+import subprocess
+import sys
+import threading
 
 import pytest
 import requests
-import socket
-import threading
 
 from anchovy.server import ThreadedHTTPServer
 from anchovy.test_harness import run_example
@@ -20,30 +23,64 @@ def get_port():
     return port
 
 
-@pytest.fixture(scope='module')
-def server(tmp_path_factory: pytest.TempPathFactory):
-    tmp_path = tmp_path_factory.mktemp('server')
-    context = run_example(EXAMPLE_PATH, tmp_path)
-    directory = context['output_dir']
-    port = get_port()
+@contextlib.contextmanager
+def run_server(directory: pathlib.Path, port: int):
     server = ThreadedHTTPServer(('localhost', port), directory)
     thread = threading.Thread(target=server.serve_forever, daemon=True)
     thread.start()
-    yield server
+    yield
     server.shutdown()
     thread.join()
 
 
-def test_server(server: ThreadedHTTPServer):
-    response = requests.get(f'http://localhost:{server.server_port}/')
+@contextlib.contextmanager
+def run_server_cli(directory: pathlib.Path, port: int):
+    args = [
+        sys.executable, '-m', 'anchovy.server',
+        '--port', str(port),
+        '--directory', str(directory)
+    ]
+    with subprocess.Popen(args) as proc:
+        yield
+        proc.terminate()
+
+
+
+@pytest.fixture(scope='module', params=[False, True])
+def server(request, tmp_path_factory: pytest.TempPathFactory):
+    tmp_path = tmp_path_factory.mktemp('server')
+    context = run_example(EXAMPLE_PATH, tmp_path)
+    directory = context['output_dir']
+    port = get_port()
+    runner = run_server if not request.param else run_server_cli
+    with runner(directory, port):
+        yield port
+
+
+def test_server(server: int):
+    response = requests.get(f'http://localhost:{server}/')
     assert response.status_code == 200
     assert response.headers['content-type'] == 'text/html'
 
 
-def test_server_etag(server: ThreadedHTTPServer):
-    response = requests.get(f'http://localhost:{server.server_port}/')
+def test_server_etag(server: int):
+    response = requests.get(f'http://localhost:{server}/')
     assert response.status_code == 200
     assert response.headers['content-type'] == 'text/html'
     etag = response.headers['etag']
-    new_response = requests.get(f'http://localhost:{server.server_port}/', headers={'If-None-Match': etag})
+    new_response = requests.get(f'http://localhost:{server}/', headers={'If-None-Match': etag})
     assert new_response.status_code == 304
+
+
+def test_server_stale_etag(server: int):
+    response = requests.get(f'http://localhost:{server}/')
+    assert response.status_code == 200
+    assert response.headers['content-type'] == 'text/html'
+    etag = response.headers['etag']
+    new_response = requests.get(f'http://localhost:{server}/', headers={'If-None-Match': etag + '0'})
+    assert new_response.status_code == 200
+
+
+def test_server_404(server: int):
+    response = requests.get(f'http://localhost:{server}/does_not_exist')
+    assert response.status_code == 404
